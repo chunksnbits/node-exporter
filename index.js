0843341842734a1ea90fa1e645be85072c6be351
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2014-08-24 12:36:22 +0200

    Transformed exporter to q promises. Added comments.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var q = require('q');
-var fs = require('fs');
+var fs = require('fs.extra');
 var _ = require('lodash');
 
 /*********************************************************
  * @name Exporter
- *
- * @type Class
  *
  * @options
  *   - cwd: The current working directory to base the
@@ -32,51 +30,59 @@ function Exporter(config) {
   }, config);
 
   this.export = function(filepath, data) {
-
     var exchange = {
       filepath: filepath,
       cwd: this.config.cwd,
       data: data
     };
 
-    return getOrCreateWorkingFilepath(exchange)
-      .then(checkFiletype)
+    return checkFiletype(exchange)
+      .then(getOrCreateWorkingFilepath)
       .then(convertData)
       .then(writeFile);
   };
 
+  // Checks the target set for the current process
+  // and creates all required directories to store
+  // the file.
   var getOrCreateWorkingFilepath = function(exchange) {
-    exchange.outputFile = cwd + '/' + exchange.filepath;
+    exchange.outputFile = exchange.cwd + '/' + exchange.filepath;
 
-    var outputPath = outputFile.replace(/\/([^\/]+)$/, '');
+    var outputPath = exchange.outputFile.replace(/\/([^\/]+)$/, '');
 
+    var deferred = q.defer();
     if (!fs.existsSync(outputPath)) {
-      var deferred = q.defer();
       fs.mkdirRecursive(outputPath, function(error) {
         if (error) {
           deferred.reject(error);
         }
         deferred.resolve(exchange);
       });
-
-      return deferred.promise;
+    } else {
+      deferred.resolve(exchange);
     }
 
-    return q(exchange);
+    return deferred.promise;
   };
 
+  // Checks the filetype based on the
+  // by looking at the file's ending.
   var checkFiletype = function(exchange) {
-    var filetype = parseFiletype(exchange.filepath);
+    exchange.filetype = parseFiletype(exchange.filepath);
 
-    if (!filetype) {
-      q.reject('Could not export data. The filetype "' + filetype + '" is not supported. Please specify an ending of: "' + _.keys(Exporter.strategies).join('", "') + '"');
+    if (!exchange.filetype) {
+      q.reject('Could not export data. The filetype "' + exchange.filetype + '" is not supported. Please specify an ending of: "' + _.keys(Exporter.strategies).join('", "') + '"');
     }
 
     return q(exchange);
   };
 
+  // Calls the strategy determined after
+  // evaluating the filetype and
+  // converts the value into the given file
+  // format.
   var convertData = function(exchange) {
-    return Exporter.strategies[filetype]
+    return Exporter.strategies[exchange.filetype]
       .convert(exchange.data)
       .then(function(convertedData) {
         exchange.data = convertedData;
@@ -84,6 +90,7 @@ function Exporter(config) {
       });
   };
 
+  // Writes the file to disk, using fs api.
   var writeFile = function(exchange) {
     var deferred = q.defer();
 
@@ -97,10 +104,13 @@ function Exporter(config) {
     return deferred.promise;
   };
 
+  // Extracts the ending
+  // Rejects the current process if no
+  // filetype could be recognized.
   var parseFiletype = function(filepath) {
     var matches = filepath.match(/\.([^.]*)$/);
     if (matches.length !== 2) {
-      throw new Error('Could not determine export type. Please specify a filetype ending on the "dest" attribute.');
+      q.reject('Could not determine export type. Please specify a filetype ending on the "dest" attribute.');
     }
 
     return matches[1];
@@ -108,7 +118,7 @@ function Exporter(config) {
 }
 
 Exporter.export = function(filepath, data, config) {
-  return new Exporter(config).export(filepath, data);
+  return (new Exporter(config)).export(filepath, data);
 };
 
 /*********************************************************
@@ -154,13 +164,20 @@ Exporter.strategies = {
   },
   json: {
     convert: function(data) {
+
+      // Creating a json string is sufficient here.
       return q(JSON.stringify(data));
     }
   },
   xml: {
     convert: function(data) {
+
       var converter = require('easyxml');
 
+      // Set layout and header options.
+      // make sure singularizeChildren
+      // is still the default, if api default changes.
+      // Set root element from 'response' to 'data'.
       converter.configure({
         singularizeChildren: true,
         rootElement: 'data',
